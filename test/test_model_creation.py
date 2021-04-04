ce552561f924f860ef1170a923166135e4a00f86
Fabian <fab.hof@gmx.de>
2021-04-04 16:02:37 +0200

    follow up: adjust test to new counting

--- test/test_model_creation.py
+++ test/test_model_creation.py
@@ -21,39 +21,39 @@ def test_add_variables_shape():
     lower = xr.DataArray(np.zeros((10,10)), coords=[range(10), range(10)])
     upper = xr.DataArray(np.ones((10, 10)), coords=[range(10), range(10)])
     m.add_variables(lower, upper)
-    assert m.variables.var1.shape == target_shape
+    assert m.variables.var0.shape == target_shape
 
     # setting only one dimension, the other has to be broadcasted
     lower = xr.DataArray(np.zeros((10)), coords=[range(10)])
     m.add_variables(lower, upper)
-    assert 'var2' in m.variables, 'Variable names are not set as expected.'
-    assert m.variables.var2.shape == target_shape
+    assert 'var1' in m.variables, 'Variable names are not set as expected.'
+    assert m.variables.var1.shape == target_shape
 
     # setting bounds without explicit bounds
     lower = xr.DataArray(np.zeros((10)))
     m.add_variables(lower, upper)
-    assert m.variables.var3.shape == target_shape
+    assert m.variables.var2.shape == target_shape
 
     # setting bounds with pandas index
     lower = xr.DataArray(np.zeros((10)), coords=[pd.Index(range(10))])
     m.add_variables(lower, upper)
-    assert m.variables.var4.shape == target_shape
+    assert m.variables.var3.shape == target_shape
 
     # define variable without any further information, should lead to a
     # single variable between minus and plus inf
     m.add_variables()
-    assert m.variables.var5.shape == ()
-    assert m.variables_lower_bound.var5 == -np.inf
-    assert m.variables_upper_bound.var5 == np.inf
+    assert m.variables.var4.shape == ()
+    assert m.variables_lower_bound.var4 == -np.inf
+    assert m.variables_upper_bound.var4 == np.inf
 
 
     # setting bounds with scalar and no coords
     lower = 0
     upper = 1
     m.add_variables(lower, upper)
-    assert m.variables.var6.shape == ()
-    assert m.variables_lower_bound.var6 == 0
-    assert m.variables_upper_bound.var6 == 1
+    assert m.variables.var5.shape == ()
+    assert m.variables_lower_bound.var5 == 0
+    assert m.variables_upper_bound.var5 == 1
 
 
     # setting bounds with scalar and coords
@@ -61,21 +61,21 @@ def test_add_variables_shape():
     upper = 1
     coords = [pd.Index(range(10)), pd.Index(range(10))]
     m.add_variables(lower, upper, coords=coords)
-    assert m.variables.var7.shape == target_shape
+    assert m.variables.var6.shape == target_shape
 
 
     # setting bounds with pd.DataFrames
     lower = pd.DataFrame(np.zeros((10, 10)))
     upper = pd.DataFrame(np.ones((10, 10)))
     m.add_variables(lower, upper)
-    assert m.variables.var8.shape == target_shape
+    assert m.variables.var7.shape == target_shape
 
 
     # setting bounds with one pd.DataFrame and one pd.Series
     lower = pd.DataFrame(np.zeros((10, 10)))
     upper = pd.Series(np.ones((10)))
     m.add_variables(lower, upper)
-    assert m.variables.var9.shape == target_shape
+    assert m.variables.var8.shape == target_shape
 
 
     # set a variable with different set of coordinates, this should be properly
@@ -83,10 +83,10 @@ def test_add_variables_shape():
     lower = pd.DataFrame(np.zeros((20, 10)))
     upper = pd.Series(np.ones((20)))
     m.add_variables(lower, upper)
-    assert m.variables.var10.shape == (20, 10)
-    # var9 should now be aligned to new coords and contain 100 nans
     assert m.variables.var9.shape == (20, 10)
-    assert m.variables.var9.notnull().sum() == 100
+    # var8 should now be aligned to new coords and contain 100 nans
+    assert m.variables.var8.shape == (20, 10)
+    assert m.variables.var8.notnull().sum() == 100
 
 
     # setting with scalar and list
@@ -96,7 +96,7 @@ def test_add_variables_shape():
 
     # repeated variable assignment is forbidden
     with pytest.raises(AssertionError):
-        m.add_variables(lower, upper, name='var9')
+        m.add_variables(lower, upper, name='var8')
 
 
 
@@ -125,11 +125,11 @@ def test_constraints():
 
     m.add_constraints(1 * x + 10 * y, '=', 0)
 
-    assert m.constraints.con1.shape == (10, 10)
-    assert m.constraints.con1.dtype == int
-    assert m.constraints_lhs_coeffs.con1.dtype in (int, float)
-    assert m.constraints_lhs_vars.con1.dtype in (int, float)
-    assert m.constraints_rhs.con1.dtype in (int, float)
+    assert m.constraints.con0.shape == (10, 10)
+    assert m.constraints.con0.dtype == int
+    assert m.constraints_lhs_coeffs.con0.dtype in (int, float)
+    assert m.constraints_lhs_vars.con0.dtype in (int, float)
+    assert m.constraints_rhs.con0.dtype in (int, float)
 
 
 def test_objective():
@@ -166,5 +166,5 @@ def test_remove_variable():
     assert 'x' not in m.variables_lower_bound
     assert 'x' not in m.variables_upper_bound
 
-    assert 1 not in m.constraints_lhs_vars.con1
+    assert 1 not in m.constraints_lhs_vars.con0
 
