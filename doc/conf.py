2aa4041841f2d97cda52f369478975dd537a96cb
Fabian <fab.hof@gmx.de>
2021-10-06 11:59:00 +0200

    dic: fix setting css file

--- doc/conf.py
+++ doc/conf.py
@@ -77,9 +77,9 @@ html_theme_options = {
 }
 
 
-# Add any paths that contain custom themes here, relative to this directory.
+# These folders are copied to the documentation's HTML output
 html_static_path = ["_static"]
 
-html_context = {
-    "css_files": ["_static/theme_overrides.css"]  # override wide tables in RTD theme
-}
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = ["theme_overrides.css"]
