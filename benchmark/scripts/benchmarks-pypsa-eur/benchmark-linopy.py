88b27087f8b8533056a8006db0042178cc02a102
Fabian <fab.hof@gmx.de>
2022-02-17 21:38:53 +0100

    benchmark: add pypsa-eur benchmarks

---
+++ benchmark/scripts/benchmarks-pypsa-eur/benchmark-linopy.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 16:20:51 2022.
+
+@author: fabian
+"""
+
+import numpy as np
+import pypsa
+from memory_profiler import profile
+
+PATH = "/home/fabian/vres/py/pypsa-eur/networks/elec_s_37.nc"
+SOLVER_PARAMS = {
+    "crossover": 0,
+    "method": 2,
+    "BarConvTol": 1.0e-3,
+    "FeasibilityTol": 1.0e-3,
+}
+
+
+@profile
+def solve():
+    n = pypsa.Network(PATH)
+    n.generators.p_nom_max.fillna(np.inf, inplace=True)
+
+    m = n.optimize(**SOLVER_PARAMS)
+
+
+solve()
