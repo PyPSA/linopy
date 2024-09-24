ce0f18574d6bf20ebabada6e5003947ea9be6963
daniel.rdt <daniel.ruedt@campus.tu-berlin.de>
2024-09-24 16:50:18 +0200

    get rid of unused type: ignore comment in solvers.py

--- linopy/solvers.py
+++ linopy/solvers.py
@@ -792,7 +792,7 @@ class Gurobi(Solver):
             def get_solver_solution() -> Solution:
                 objective = m.ObjVal
 
-                sol = pd.Series({v.VarName: v.x for v in m.getVars()}, dtype=float)  # type: ignore
+                sol = pd.Series({v.VarName: v.x for v in m.getVars()}, dtype=float)
                 sol = set_int_index(sol)
 
                 try:
