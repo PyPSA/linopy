8363b6acf318c00cbb8f63dad956323483d64c46
ollie-bell <56110893+ollie-bell@users.noreply.github.com>
2025-02-09 10:51:31 +0000

    fix: polars schema Int64 windows numpy > 2

--- linopy/common.py
+++ linopy/common.py
@@ -8,6 +8,7 @@ This module contains commonly used functions.
 from __future__ import annotations
 
 import operator
+import os
 from collections.abc import Generator, Hashable, Iterable, Sequence
 from functools import reduce, wraps
 from pathlib import Path
@@ -317,9 +318,11 @@ def infer_schema_polars(ds: Dataset) -> dict[Hashable, pl.DataType]:
         dict: A dictionary mapping column names to their corresponding Polars data types.
     """
     schema = {}
+    np_major_version = int(np.__version__.split(".")[0])
+    use_int32 = os.name == "nt" and np_major_version < 2
     for name, array in ds.items():
         if np.issubdtype(array.dtype, np.integer):
-            schema[name] = pl.Int64
+            schema[name] = pl.Int32 if use_int32 else pl.Int64
         elif np.issubdtype(array.dtype, np.floating):
             schema[name] = pl.Float64  # type: ignore
         elif np.issubdtype(array.dtype, np.bool_):
