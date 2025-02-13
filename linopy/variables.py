03007f38e66fdedbd3f76abab0d5ba8dfd977fae
FBumann <117816358+FBumann@users.noreply.github.com>
2025-02-13 12:27:32 +0100

    Return not implemented if to_linexpr() fails

--- linopy/variables.py
+++ linopy/variables.py
@@ -314,7 +314,10 @@ class Variable:
         linopy.LinearExpression
             Linear expression with the variables and coefficients.
         """
-        coefficient = as_dataarray(coefficient, coords=self.coords, dims=self.dims)
+        try:
+            coefficient = as_dataarray(coefficient, coords=self.coords, dims=self.dims)
+        except TypeError:
+            return NotImplemented
         ds = Dataset({"coeffs": coefficient, "vars": self.labels}).expand_dims(
             TERM_DIM, -1
         )
