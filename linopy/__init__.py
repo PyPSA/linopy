99b2bb70ff825426443a7b469596f8be9b33cba7
FBumann <117816358+FBumann@users.noreply.github.com>
2025-02-18 18:15:10 +0100

    Add Constraints and Variables to Public API (#419)

--- linopy/__init__.py
+++ linopy/__init__.py
@@ -14,15 +14,16 @@ __version__ = version("linopy")
 import linopy.monkey_patch_xarray  # noqa: F401
 from linopy.config import options
 from linopy.constants import EQUAL, GREATER_EQUAL, LESS_EQUAL
-from linopy.constraints import Constraint
+from linopy.constraints import Constraint, Constraints
 from linopy.expressions import LinearExpression, QuadraticExpression, merge
 from linopy.io import read_netcdf
-from linopy.model import Model, Variable, available_solvers
+from linopy.model import Model, Variable, Variables, available_solvers
 from linopy.objective import Objective
 from linopy.remote import RemoteHandler
 
 __all__ = (
     "Constraint",
+    "Constraints",
     "EQUAL",
     "GREATER_EQUAL",
     "LESS_EQUAL",
@@ -32,6 +33,7 @@ __all__ = (
     "QuadraticExpression",
     "RemoteHandler",
     "Variable",
+    "Variables",
     "available_solvers",
     "merge",
     "options",
