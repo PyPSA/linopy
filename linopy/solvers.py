07c9d1347862870fdb3a21df8a93fc14ddf427e9
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2025-04-15 22:03:03 +0200

    fix: don't disable presolve and fix dual value retrieval for SCIP (#441)
    * SCIP: Don't disable presolve, fix dual value retrieval
    
    * Remove erraneously added comment
    
    * Also ignore quadobjvar and qmatrixvar from dual solution
    
    * Update release notes
    
    * Amend the list of vars to ignore in SCIP's solution

--- linopy/solvers.py
+++ linopy/solvers.py
@@ -471,6 +471,7 @@ class CBC(Solver):
 
             sol = df[variables_b][2]
             dual = df[~variables_b][3]
+
             return Solution(sol, dual, objective)
 
         solution = self.safe_get_solution(status=status, func=get_solver_solution)
@@ -1341,9 +1342,6 @@ class SCIP(Solver):
         if warmstart_fn:
             logger.warning("Warmstart not implemented for SCIP")
 
-        # In order to retrieve the dual values, we need to turn off presolve
-        m.setPresolve(scip.SCIP_PARAMSETTING.OFF)
-
         m.optimize()
 
         if basis_fn:
@@ -1362,21 +1360,24 @@ class SCIP(Solver):
 
         def get_solver_solution() -> Solution:
             objective = m.getObjVal()
+            vars_to_ignore = {"quadobjvar", "qmatrixvar", "quadobj", "qmatrix"}
 
             s = m.getSols()[0]
-            sol = pd.Series({v.name: s[v] for v in m.getVars()})
-            sol.drop(
-                ["quadobjvar", "qmatrixvar"], errors="ignore", inplace=True, axis=0
+            sol = pd.Series(
+                {v.name: s[v] for v in m.getVars() if v.name not in vars_to_ignore}
             )
 
-            cons = m.getConss()
+            cons = m.getConss(False)
             if len(cons) != 0:
-                dual = pd.Series({c.name: m.getDualSolVal(c) for c in cons})
-                dual = dual[
-                    dual.index.str.startswith("c") & ~dual.index.str.startswith("cf")
-                ]
+                dual = pd.Series(
+                    {
+                        c.name: m.getDualSolVal(c)
+                        for c in cons
+                        if c.name not in vars_to_ignore
+                    }
+                )
             else:
-                logger.warning("Dual values of MILP couldn't be parsed")
+                logger.warning("Dual values not available (is this an MILP?)")
                 dual = pd.Series(dtype=float)
 
             return Solution(sol, dual, objective)
