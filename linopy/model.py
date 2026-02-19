fea8f9b1c49dde14a038742e82a34a919f52594d
FBumann <117816358+FBumann@users.noreply.github.com>
2026-02-19 09:23:02 +0100

    ensure mask broadcasting still works as expected, even with misaligned coords

--- linopy/model.py
+++ linopy/model.py
@@ -574,7 +574,10 @@ class Model:
         self._check_valid_dim_names(data)
 
         if mask is not None:
-            mask = as_dataarray(mask, coords=data.coords, dims=data.dims).astype(bool)
+            # TODO: Simplify when removing Future Warning from broadcast_mask
+            if not isinstance(mask, DataArray):
+                mask = as_dataarray(mask, coords=data.coords, dims=data.dims)
+            mask = mask.astype(bool)
             mask = broadcast_mask(mask, data.labels)
 
         # Auto-mask based on NaN in bounds (use numpy for speed)
@@ -799,7 +802,10 @@ class Model:
         (data,) = xr.broadcast(data, exclude=[TERM_DIM])
 
         if mask is not None:
-            mask = as_dataarray(mask, coords=data.coords, dims=data.dims).astype(bool)
+            # TODO: Simplify when removing Future Warning from broadcast_mask
+            if not isinstance(mask, DataArray):
+                mask = as_dataarray(mask, coords=data.coords, dims=data.dims)
+            mask = mask.astype(bool)
             mask = broadcast_mask(mask, data.labels)
 
         # Auto-mask based on null expressions or NaN RHS (use numpy for speed)
